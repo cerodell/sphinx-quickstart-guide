f8c0e48e1ebdbbf64285f829054a564cc379dbcf
Chris Rodell <crodell@eoas.ubc.ca>
2020-09-18 11:51:08 -0700

    updates to markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,9 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../../'))
 
-
+def setup(app):
+    app.add_stylesheet('css/custom.css')
+    
 # -- Project information -----------------------------------------------------
 
 project = 'Sphinx Quick Start Guide'
