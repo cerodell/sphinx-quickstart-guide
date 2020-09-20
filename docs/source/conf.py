d679a3637cce0c97ca5404b59af2ac0fdc9aa3aa
Chris Rodell <crodell@eoas.ubc.ca>
2020-09-19 21:03:54 -0700

    new logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,4 +63,5 @@ html_theme = 'sphinx_book_theme'
 # # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
+html_title = "Sphinx Quick Start Guide"
+html_logo = "_static/img/dojocat.jpg"
